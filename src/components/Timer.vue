0809b9e9dd9e2f31fdcd60633c17cc83860a6048
Vitor Loddi <vitor_loddi@hotmail.com>
2023-01-28 17:18:50 -0300

    feat(components/Timer.vue): Add stopwatch and button set

---
+++ src/components/Timer.vue
@@ -0,0 +1,74 @@
+<template>
+	<div class="is-flex is-align-items-center is-justify-content-space-between">
+		<stopwatch :time="timeSeconds" />
+
+		<base-button-with-icon
+			text="start"
+			icon="fa-play"
+			:buttonDisabled="timerIsActive"
+			@buttonClicked="start"
+		/>
+
+		<base-button-with-icon
+			text="stop"
+			icon="fa-stop"
+			:buttonDisabled="!timerIsActive"
+			@buttonClicked="finalize"
+		/>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Stopwatch from '@/components/Stopwatch.vue';
+import BaseButtonWithIcon from '@/components/Base/BaseButtonWithIcon.vue';
+
+export default defineComponent({
+	name: 'Timer',
+
+	emits: [
+		'timerStarted',
+		'timerFinalized',
+	],
+
+	components: {
+		Stopwatch,
+		BaseButtonWithIcon,
+	},
+
+	data() {
+		return {
+			timeSeconds: 0,
+			stopwatch: 0,
+		};
+	},
+
+	computed: {
+		timerIsActive() : boolean {
+			return !!this.timeSeconds;
+		},
+	},
+
+	methods: {
+		start() : void {
+			this.$emit('timerStarted');
+
+			this.stopwatch = setInterval(() => {
+				this.timeSeconds += 1;
+			}, 1000);
+		},
+
+		finalize() : void {
+			clearInterval(this.stopwatch);
+
+			this.$emit('timerFinalized', this.timeSeconds);
+
+			this.timeSeconds = 0;
+		},
+	},
+});
+</script>
+
+<style>
+
+</style>
