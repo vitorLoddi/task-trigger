847124a851c08265ee32ab82459c3267e67358f7
Vitor Loddi <vitor_loddi@hotmail.com>
2023-01-28 17:17:13 -0300

    feat(components/Stopwatch.vue): Add component responsible for displaying elapsed time

---
+++ src/components/Stopwatch.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="holder-time">
+		<strong>{{ elapsedTime }}</strong>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'Stopwatch',
+
+	props: {
+		time: {
+			type: Number,
+			default: 0,
+		},
+	},
+
+	computed: {
+		elapsedTime() : string {
+			return new Date(this.time * 1000).toISOString().substr(11, 8);
+		},
+	},
+});
+</script>
