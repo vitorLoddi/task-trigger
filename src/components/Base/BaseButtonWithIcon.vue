17a45cf663f1a1dd4ebfc1afd8b00641b8e175de
Vitor Loddi <vitor_loddi@hotmail.com>
2023-01-28 17:14:46 -0300

    feat(Base/BaseButtonWithIcon.vue): Add icon button display component

---
+++ src/components/Base/BaseButtonWithIcon.vue
@@ -0,0 +1,45 @@
+<template>
+	<button
+		class="button"
+		:disabled="buttonDisabled"
+		@click="$emit('buttonClicked')"
+	>
+		<span class="icon">
+			<i :class="`fas ${icon}`"></i>
+		</span>
+
+		<span>
+			{{ text }}
+		</span>
+	</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'BaseButtonWithIcon',
+
+	emits: [
+		'buttonClicked',
+	],
+
+	props: {
+		icon: {
+			type: String,
+			required: true,
+		},
+
+		text: {
+			type: String,
+			required: true,
+		},
+
+		buttonDisabled: {
+			type: Boolean,
+			default: false,
+		},
+	},
+});
+
+</script>
