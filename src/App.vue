f7ab9a859ab1835b40f7fcf5e913420d40bd47e6
Vitor Loddi <vitor_loddi@hotmail.com>
2023-01-30 07:17:48 -0300

    feat(App.vue): Realization and application of components to create the tasks page

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+	<main class="columns is-gapless is-multline">
+		<div class="column is-one-quarter">
+			<sidebar />
+		</div>
+
+		<div class="column is-three-quarter">
+			<task-form
+				@saveTask="saveTask"
+			/>
+
+			<div class="list">
+				<task
+					v-for="(task, index) in toDoList"
+					:key="index"
+					:task="task"
+				/>
+
+				<box
+					v-if="listIsEmpty"
+				>
+					<p>Você não está muito produtivo hoje :(</p>
+				</box>
+			</div>
+		</div>
+	</main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+
+// Components
+import Sidebar from '@/components/Sidebar.vue';
+import TaskForm from '@/components/TaskForm.vue';
+import Task from '@/components/Task.vue';
+import Box from '@/components/Box.vue';
+
+// Interfaces
+import ITask from '@/interfaces/ITask';
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
+	name: 'App',
+
+	components: {
+		Sidebar,
+		TaskForm,
+		Task,
+		Box,
+	},
+
+	data() {
+		return {
+			toDoList: [] as ITask[],
+		};
+	},
+
+	computed: {
+		listIsEmpty() : boolean {
+			return !this.toDoList.length;
+		},
+	},
+
+	methods: {
+		saveTask(task : ITask) {
+			this.toDoList.push(task);
+		},
+	},
 });
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+.list {
+	padding: 1.25rem;
 }
 </style>
