acea5f3e1fd48a98d14f1f8f5a2323093ede9091
Vitor Loddi <vitor_loddi@hotmail.com>
2023-01-30 07:13:26 -0300

    feat(components/Box.vue): Add task info box

---
+++ src/components/Box.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="box has-text-weight-bold">
+		<slot />
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'Box',
+
+});
+</script>
+
+<style lang="scss" scoped>
+.box {
+	background: #FAFCAF;
+}
+</style>
