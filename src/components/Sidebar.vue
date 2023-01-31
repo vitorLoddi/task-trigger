81b03258d463693d0660c193724a596d334bc987
Vitor Loddi <vitor_loddi@hotmail.com>
2023-01-31 07:25:48 -0300

    feat(Components/Sidebar): Add dark mode option

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,6 +1,12 @@
 <template>
 	<header>
-		<img src="../assets/logo.png" alt="Logo" width="80" height="80">
+		<div>
+			<img src="../assets/logo.png" alt="Logo" width="80" height="80">
+		</div>
+
+		<button class="btn-dark-mode" @click="changeTheme">
+			{{  darkModeActive ? 'Ativar' : 'Desativar ' }} modo escuro
+		</button>
 	</header>
 </template>
 
@@ -9,21 +15,60 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
 	name: 'Sidebar',
+
+	emits: [
+		'changeTheme',
+	],
+
+	data() {
+		return {
+			darkModeActive: false,
+		};
+	},
+
+	methods: {
+		changeTheme() : void {
+			this.darkModeActive = !this.darkModeActive;
+
+			this.$emit('changeTheme', this.darkModeActive);
+		},
+	},
 });
 </script>
 
-<style lang="scss">
-header {
-	padding: 1rem;
-	background: #0d3b66;
-	width: 100%;
-	height: 100vh;
-}
-
-@media only screen and (max-width: 768px) {
+<style lang="scss" scoped>
 	header {
-		padding: 2.5rem;
-		height: auto;
+		padding: 1rem;
+		background: #0d3b66;
+		width: 100%;
+		height: 100vh;
+		text-align: center;
 	}
-}
+
+	button {
+		width: 180px;
+		height: 34px;
+		border: none;
+		border-radius: 6px;
+		margin-top: 40px;
+		box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+		transition: all 0.3s ease 0s;
+		cursor: pointer;
+		outline: none;
+		font-weight: bold;
+
+		&:hover {
+			background-color: #0dc279;
+			box-shadow: 0px 15px 20px rgba(46, 229, 157, 0.4);
+			color: #fff;
+			transform: translateY(-7px);
+		}
+	}
+
+	@media only screen and (max-width: 768px) {
+		header {
+			padding: 2.5rem;
+			height: auto;
+		}
+	}
 </style>
