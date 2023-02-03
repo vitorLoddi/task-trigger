9f255b11900d2577600d1b6007985a79610a2ba1
Vitor Loddi <vitor_loddi@hotmail.com>
2023-02-03 07:19:30 -0300

    feat(main.ts): Use voue-router

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue';
+import router from '@/router/index';
 import App from './App.vue';
 
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
-createApp(App).mount('#app');
+createApp(App)
+	.use(router)
+	.mount('#app');
