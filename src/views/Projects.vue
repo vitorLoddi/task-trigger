42ce97866b98039d90616b439b753313c093c084
Vitor Loddi <vitor_loddi@hotmail.com>
2023-02-06 07:41:57 -0300

    refactor(views/Projects.vue): Remove form from this view

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -2,34 +2,20 @@
 	<section class="projects">
 		<h1 class="title">Projetos</h1>
 
-		<form @submit.prevent="saveProject">
-			<div class="field">
-				<label
-					for="nameProject"
-					class="label"
-				>
-					Nome do Projeto:
-					<input
-						type="text"
-						id="nameProject"
-						class="input"
-						v-model="nameProject"
-					>
-				</label>
-			</div>
+		<router-link to="/projetos/novo" class="button">
+			<span class="icon is-small">
+				<i class="fas fa-plus"></i>
+			</span>
 
-			<div class="field">
-				<button type="submit" class="button">
-					Salvar
-				</button>
-			</div>
-		</form>
+			<span>Novo Projeto</span>
+		</router-link>
 
 		<table class="table is-fullwidth">
 			<thead>
 				<tr>
 					<th>ID</th>
 					<th>Nome do Projeto</th>
+					<th>Ações</th>
 				</tr>
 			</thead>
 
@@ -40,6 +26,13 @@
 				>
 					<td>{{ project.id }}</td>
 					<td>{{ project.name }}</td>
+					<td>
+						<router-link :to="`/projetos/${project.id}`" class="button">
+							<span class="icon is-small">
+								<i class="fas fa-pencil-alt"></i>
+							</span>
+						</router-link>
+					</td>
 				</tr>
 			</tbody>
 		</table>
@@ -55,28 +48,10 @@ import { useStore } from '@/store';
 export default defineComponent({
 	name: 'Projects',
 
-	data() {
-		return {
-			nameProject: '',
-		};
-	},
-
-	methods: {
-		saveProject() {
-			this.store.commit(
-				'ADD_PROJECT',
-				this.nameProject,
-			);
-
-			this.nameProject = '';
-		},
-	},
-
 	setup() {
 		const store = useStore();
 
 		return {
-			store,
 			projects: computed(() => store.state.projects),
 		};
 	},
