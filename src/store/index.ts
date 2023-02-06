dca41f6c8f52cef44f0de45c1d07c07d8ece88d2
Vitor Loddi <vitor_loddi@hotmail.com>
2023-02-06 07:29:00 -0300

    feat(store/index.ts): State creation and management by vuex

---
+++ src/store/index.ts
@@ -0,0 +1,30 @@
+import IProject from '@/interfaces/IProject';
+import { InjectionKey } from 'vue';
+import { createStore, Store, useStore as vuexUseStore } from 'vuex';
+
+interface State {
+	projects: IProject[]
+}
+
+export const key: InjectionKey<Store<State>> = Symbol('Key');
+
+export const store = createStore<State>({
+	state: {
+		projects: [],
+	},
+
+	mutations: {
+		ADD_PROJECT(state, nameProject: string) {
+			const project = {
+				id: new Date().toISOString(),
+				name: nameProject,
+			} as IProject;
+
+			state.projects.push(project);
+		},
+	},
+});
+
+export function useStore(): Store<State> {
+	return vuexUseStore(key);
+}
