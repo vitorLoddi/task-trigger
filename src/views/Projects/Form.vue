e428febe1ba29f6f9157e84ae8efca8d6b5a32a8
Vitor Loddi <vitor_loddi@hotmail.com>
2023-02-06 07:41:03 -0300

    refactor(Projects/Form.vue): Add view only for registering new projects

---
+++ src/views/Projects/Form.vue
@@ -0,0 +1,72 @@
+<template>
+	<section class="projects">
+		<h1 class="title">Projetos</h1>
+
+		<form @submit.prevent="saveProject">
+			<div class="field">
+				<label
+					for="nameProject"
+					class="label"
+				>
+					Nome do Projeto:
+					<input
+						type="text"
+						id="nameProject"
+						class="input"
+						v-model="nameProject"
+					>
+				</label>
+			</div>
+
+			<div class="field">
+				<button type="submit" class="button">
+					Salvar
+				</button>
+			</div>
+		</form>
+	</section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+// Interfaces
+import { useStore } from '@/store';
+
+export default defineComponent({
+	name: 'Form',
+
+	data() {
+		return {
+			nameProject: '',
+		};
+	},
+
+	methods: {
+		saveProject() {
+			this.store.commit(
+				'ADD_PROJECT',
+				this.nameProject,
+			);
+
+			this.nameProject = '';
+
+			this.$router.push('/projetos');
+		},
+	},
+
+	setup() {
+		const store = useStore();
+
+		return {
+			store,
+		};
+	},
+});
+</script>
+
+<style lang="scss" scoped>
+	.projects {
+		padding: 1.25rem;
+	}
+</style>
