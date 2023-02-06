6a2b2c4197afb6db99144ccbc3f3ac18838e3dc8
Vitor Loddi <vitor_loddi@hotmail.com>
2023-02-06 07:22:21 -0300

    feat(main.ts): Use vuex

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
-import { createApp } from 'vue';
 import router from '@/router/index';
+import { key, store } from '@/store';
+
+import { createApp } from 'vue';
 import App from './App.vue';
 
+// Style
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 createApp(App)
 	.use(router)
+	.use(store, key)
 	.mount('#app');
