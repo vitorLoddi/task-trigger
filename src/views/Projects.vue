57125912a90bad7af3cd510f9e457444f88504f2
Vitor Loddi <vitor_loddi@hotmail.com>
2023-02-09 07:18:31 -0300

    refactor(views/Projects.vue): Decoupling content

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,60 +1,16 @@
 <template>
-	<section class="projects">
+	<div class="projects">
 		<h1 class="title">Projetos</h1>
 
-		<router-link to="/projetos/novo" class="button">
-			<span class="icon is-small">
-				<i class="fas fa-plus"></i>
-			</span>
-
-			<span>Novo Projeto</span>
-		</router-link>
-
-		<table class="table is-fullwidth">
-			<thead>
-				<tr>
-					<th>ID</th>
-					<th>Nome do Projeto</th>
-					<th>Ações</th>
-				</tr>
-			</thead>
-
-			<tbody>
-				<tr
-					v-for="(project, index) in projects"
-					:key="index"
-				>
-					<td>{{ project.id }}</td>
-					<td>{{ project.name }}</td>
-					<td>
-						<router-link :to="`/projetos/${project.id}`" class="button">
-							<span class="icon is-small">
-								<i class="fas fa-pencil-alt"></i>
-							</span>
-						</router-link>
-					</td>
-				</tr>
-			</tbody>
-		</table>
-	</section>
+		<router-view />
+	</div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue';
-
-// Interfaces
-import { useStore } from '@/store';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
 	name: 'Projects',
-
-	setup() {
-		const store = useStore();
-
-		return {
-			projects: computed(() => store.state.projects),
-		};
-	},
 });
 </script>
 
