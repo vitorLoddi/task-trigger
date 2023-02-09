2af29e9aa2e6fe76242ad8b73e5feee0cc4c1572
Vitor Loddi <vitor_loddi@hotmail.com>
2023-02-09 08:22:27 -0300

    feat(components/Notifications.vue): Add component responsible for issuing alerts

---
+++ src/components/Notifications.vue
@@ -0,0 +1,55 @@
+<template>
+	<div class="notifications">
+		<article
+			v-for="notification in notifications"
+			:key="notification.id"
+			:class="`message ${situation[notification.type]}`"
+		>
+			<div class="message-header">
+				{{ notification.title }}
+			</div>
+
+			<div class="message-body">
+				{{ notification.text }}
+			</div>
+		</article>
+	</div>
+</template>
+
+<script lang="ts">
+import { TypeNotification } from '@/interfaces/INotification';
+import { useStore } from '@/store';
+import { computed, defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'Notifications',
+
+	data() {
+		return {
+			situation: {
+				[TypeNotification.SUCESS]: 'is-success',
+				[TypeNotification.ATTENTION]: 'is-warning',
+				[TypeNotification.FAILURE]: 'is-danger',
+			},
+		};
+	},
+
+	setup() {
+		const store = useStore();
+
+		return {
+			notifications: computed(() => store.state.notifications),
+		};
+	},
+});
+</script>
+
+<style lang="scss" scoped>
+	.notifications {
+		position: absolute;
+		right: 0;
+		width: 300px;
+		padding: 8px;
+		z-index: 2;
+	}
+</style>
