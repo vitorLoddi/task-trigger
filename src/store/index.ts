f146010d31580364502c3cdc778b5a8898e26bf2
Vitor Loddi <vitor_loddi@hotmail.com>
2023-02-09 07:26:43 -0300

    refactor(store/index.ts): Alias creation and delete functionality

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
 import IProject from '@/interfaces/IProject';
 import { InjectionKey } from 'vue';
 import { createStore, Store, useStore as vuexUseStore } from 'vuex';
+import { ADD_PROJECT, CHANGE_PROJECT, DELETE_PROJECT } from '@/store/type-mutations';
 
 interface State {
 	projects: IProject[]
@@ -14,7 +15,7 @@ export const store = createStore<State>({
 	},
 
 	mutations: {
-		ADD_PROJECT(state, nameProject: string) {
+		[ADD_PROJECT](state, nameProject: string) {
 			const project = {
 				id: new Date().toISOString(),
 				name: nameProject,
@@ -23,9 +24,17 @@ export const store = createStore<State>({
 			state.projects.push(project);
 		},
 
-		CHANGE_PROJECT(state, project: IProject) {
-			const index = state.projects.findIndex((proj) => proj.id === project.id);
+		[CHANGE_PROJECT](state, project: IProject) {
+			const index = state.projects.findIndex(
+				(proj) => proj.id === project.id,
+			);
 			state.projects[index] = project;
+		},
+
+		[DELETE_PROJECT](state, idProject : string) {
+			state.projects = state.projects.filter(
+				(proj) => proj.id !== idProject,
+			);
 		},
 	},
 });
