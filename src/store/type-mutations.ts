339152cd20b00a0736549968d184cce2320d9cfc
Vitor Loddi <vitor_loddi@hotmail.com>
2023-04-24 12:30:24 -0300

    feat(store/type-mutations.ts): Add new token to set projects

--- src/store/type-mutations.ts
+++ src/store/type-mutations.ts
@@ -1,5 +1,6 @@
 export const ADD_PROJECT = 'ADD_PROJECT';
 export const CHANGE_PROJECT = 'CHANGE_PROJECT';
 export const DELETE_PROJECT = 'DELETE_PROJECT';
+export const SET_PROJECTS = 'SET_PROJECTS';
 
 export const NOTIFY = 'NOTIFY';
